3a25da3066fc544153cc66f84786afae924c7227
unknown <occ25@icloud.com>
2021-11-18 15:40:14 -0500

    New Deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env  sh
+
+set -e
+
+npm run build
+
+git init
+git add -A
+git commit -m 'New Deploy'
+git push -f  https://github.com/boss-oma/vuejs-pages.git master:gh-pages
+
+cd -
---
+++ vuejs.config.js
@@ -0,0 +1,4 @@
+module.export = {
+    publicPath:'/vuejs-pages'
+}
+
